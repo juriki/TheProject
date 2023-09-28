34af488c2fe898be4379938a4246dc06b1e4d82d
juriki <juritokvin@gmailcom>
2023-09-28 08:59:37 +0300

    Initial Commit

---
+++ TheProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheProject
+//
+//  Created by Jurij Tokvin on 28.9.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheProject/TheProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheProjectApp.swift
+//  TheProject
+//
+//  Created by Jurij Tokvin on 28.9.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
